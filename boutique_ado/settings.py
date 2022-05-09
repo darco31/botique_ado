91d278bd04644d58541ca57c4e2f2c04cc61f48a
Stephen <sdarcy29@hotmail.com>
2022-05-09 19:52:56 +0000

    Add search functionality to site

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -143,3 +143,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+free_shipping_threshold = 40
