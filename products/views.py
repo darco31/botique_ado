82e8753ba12a74ccfb99208d5cef3ee3408555cc
Stephen <sdarcy29@hotmail.com>
2022-05-12 19:22:56 +0000

    Add urls for all products in the dropdown menus

--- products/views.py
+++ products/views.py
@@ -12,7 +12,7 @@ def all_products(request):
     products = Product.objects.all()
 
     query = None
-    category = None
+    categories = None
 
     if request.GET:
         if 'category' in request.GET:
@@ -32,7 +32,7 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
-        'current categories': categories, 
+        'current categories': categories,
     }
 
     return render(request, 'products/products.html', context)
